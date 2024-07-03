f957a98b17f6932244315d79d677d07ee56800c9
EliPeixoto <eliane.peixoto@outlook.com>
2024-07-03 19:52:23 -0300

    criando entidade parquimetro

---
+++ src/main/java/com/fiap/parquimetro/entities/parquimetro.java
@@ -0,0 +1,23 @@
+package com.fiap.parquimetro.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Data
+public class parquimetro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String localizacao;
+    private String status;
+    private Integer capacidadeVagas;
+    private LocalDateTime dataInstalacao;
+}
