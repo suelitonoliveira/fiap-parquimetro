7d8119fc6d0f5f463e8454020d305de61de38657
EliPeixoto <eliane.peixoto@outlook.com>
2024-07-03 23:12:07 -0300

    alterado nomelnclatura das colunas das entidades Condutor e Endereco

--- src/main/java/com/fiap/parquimetro/entities/Endereco.java
+++ src/main/java/com/fiap/parquimetro/entities/Endereco.java
@@ -11,11 +11,22 @@ public class Endereco {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "logradouro", length = 100)
     private String logradouro;
+
+    @Column(name = "numero")
     private Integer numero;
+
+    @Column(name = "complemento", length = 50)
     private String complemento;
+
+    @Column(name = "cidade", length = 50)
     private String cidade;
+
+    @Column(name = "estado", length = 2)
     private String estado;
+
+    @Column(name = "cep", length = 8)
     private String cep;
 
 
