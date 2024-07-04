a7273d23292d34515706b9e4508bc17f2a8d7e7b
EliPeixoto <eliane.peixoto@outlook.com>
2024-07-03 21:18:50 -0300

    add classe e update
    criando classe Endereco e atualizando classe condutor

--- src/main/java/com/fiap/parquimetro/entities/Condutor.java
+++ src/main/java/com/fiap/parquimetro/entities/Condutor.java
@@ -17,6 +17,8 @@ public class Condutor {
     @Column(nullable = false)
     private String nome;
 
+    private Endereco endereco;
+
     @Column(nullable = false, unique = true)
     private String cpf;
 
