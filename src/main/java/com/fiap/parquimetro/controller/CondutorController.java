935360adc151670bb9534e9e2caf26ae8fa537c8
EliPeixoto <eliane.peixoto@outlook.com>
2024-07-06 20:46:21 -0300

    adicionando documentacao swagger

--- src/main/java/com/fiap/parquimetro/controller/CondutorController.java
+++ src/main/java/com/fiap/parquimetro/controller/CondutorController.java
@@ -3,6 +3,7 @@ package com.fiap.parquimetro.controller;
 import com.fiap.parquimetro.entities.Condutor;
 import com.fiap.parquimetro.repositories.CondutorRepository;
 import com.fiap.parquimetro.services.CondutorService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -23,12 +24,13 @@ public class CondutorController {
         this.condutorRepository = condutorRepository;
     }
 
+    @Operation(summary= "Lista todos os condutores", tags = "Condutor")
     @GetMapping
     public ResponseEntity<List<Condutor>> listarTodos() {
         List<Condutor> condutors = this.condutorService.listarTodos();
         return ResponseEntity.ok(condutors);
     }
-
+    @Operation(summary= "Cadastra Novo Condutor", tags = "Condutor")
     @PostMapping
     public ResponseEntity<Condutor> cadastrarCondutor(@RequestBody Condutor condutor) {
         if (condutor.getEndereco() == null) {
