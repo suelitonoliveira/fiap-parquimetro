6ae5e8861044aeec93b4d4b7a74263745a7b8d8c
SUELITON DE OLIVEIRA (X01142591190) <x01142591190@tcu.gov.br>
2024-07-22 18:51:51 -0300

    ajustando validações de alguns campos faltantes

--- src/main/java/com/fiap/parquimetro/dto/EnderecoDTO.java
+++ src/main/java/com/fiap/parquimetro/dto/EnderecoDTO.java
@@ -3,6 +3,7 @@ package com.fiap.parquimetro.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,15 +25,20 @@ public class EnderecoDTO {
     private Integer numero;
 
     @NotBlank(message = "Campo complemento obrigatório")
+    @Size(max = 50, message = "O campo cidade deve ter no máximo 50 caracteres")
     private String complemento;
 
     @NotBlank(message = "Campo cidade obrigatório")
+    @Size(max = 50, message = "O campo cidade deve ter no máximo 50 caracteres")
     private String cidade;
 
     @NotBlank(message = "Campo estado obrigatório")
+    @Size(max = 2, message = "O campo estado deve ter no máximo 2 caracteres")
+    @Schema(description = "Estado representado por sua sigla", example = "SP", required = true)
     private String estado;
 
     @NotBlank(message = "Campo cep obrigatório")
+    @Size(max = 8, message = "O campo cep deve ter no máximo 8 caracteres")
     private String cep;
 
 
--- src/main/java/com/fiap/parquimetro/dto/UsuarioDTO.java
+++ src/main/java/com/fiap/parquimetro/dto/UsuarioDTO.java
@@ -1,16 +1,14 @@
 package com.fiap.parquimetro.dto;
 
-import com.fiap.parquimetro.entities.Endereco;
-import com.fiap.parquimetro.entities.Usuario;
 import com.fiap.parquimetro.entities.Veiculo;
 import com.fiap.parquimetro.enums.TipoUsuario;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
 
-import java.util.Date;
 import java.util.List;
 
 @Data
@@ -26,9 +24,11 @@ public class UsuarioDTO {
     private Long codEndereco;
 
     @NotBlank(message = "Campo cpf obrigatório")
+    @CPF
     private String cpf;
 
     @NotBlank(message = "Campo email obrigatório")
+    @Email
     private String email;
 
     @NotBlank(message = "Campo telefone obrigatório")
