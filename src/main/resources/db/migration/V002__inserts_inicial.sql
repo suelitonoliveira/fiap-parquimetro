b9b50b61c7b5f27d26c6915ef824ff4b4ed2cfcf
SUELITON DE OLIVEIRA (X01142591190) <x01142591190@tcu.gov.br>
2024-07-23 18:06:02 -0300

    alterando tamanho da coluna do cep

--- src/main/resources/db/migration/V002__inserts_inicial.sql
+++ src/main/resources/db/migration/V002__inserts_inicial.sql
@@ -1,5 +1,5 @@
 INSERT INTO parquimetro.tb_endereco(logradouro, numero, complemento, cidade, estado, cep)
-values ('teste',12,'completo','brasilia','df','qualquer');
+values ('teste',12,'completo','brasilia','df','qua');
 INSERT INTO parquimetro.tb_usuario (nome, cod_endereco, cpf, email, telefone, tipo_usuario)
 values ('admin',1,'76961801005','email@teste.com.br','61995478745','ADMIN_SISTEMA'  );
 INSERT INTO parquimetro.tb_usuario (nome, cod_endereco, cpf, email, telefone, tipo_usuario)
